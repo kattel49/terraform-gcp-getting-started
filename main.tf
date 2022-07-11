81ef2caa2f62ade740203559a833f67a895e6177
kattel49 <shubhushankattel@gmail.com>
2022-07-11 16:35:40 +0200

    Addition: Custom VPC network and a subnet in europe-west-3

--- main.tf
+++ main.tf
@@ -13,3 +13,17 @@ provider "google" {
     region = "europe-west3"
     zone = "europe-west3-a"
 }
+
+#custom network
+resource "google_compute_network" "vpc_network" {
+    name = "terraform-network"
+    auto_create_subnetworks = false
+}
+
+#subnetwork
+resource "google_compute_subnetwork" "terraform_subnet_eur" {
+    name = "terraform-subnet-eur"
+    ip_cidr_range = "10.1.0.0/16"
+    #attach subnetwork to the vpc_network defined above
+    network = google_compute_network.vpc_network.id
+}
